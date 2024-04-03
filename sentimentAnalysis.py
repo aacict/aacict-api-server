ce043101b8999ccfef13ad3e6de640049923acf5
Ashish Thapa <aacict@gmail.com>
2024-04-03 17:23:15 -0400

    sentiment analysis using twitter-roberta-base-sentiment-latest transformer

---
+++ sentimentAnalysis.py
@@ -0,0 +1,36 @@
+from transformers import AutoModelForSequenceClassification
+from transformers import TFAutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoConfig
+import numpy as np
+from scipy.special import softmax
+
+def preprocess(text):
+    new_text = []
+    for t in text.split(" "):
+        t = '@user' if t.startswith('@') and len(t) > 1 else t
+        t = 'http' if t.startswith('http') else t
+        new_text.append(t)
+    return " ".join(new_text)
+
+
+def getSentimentFromText(text):
+    MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
+    tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    config = AutoConfig.from_pretrained(MODEL)
+
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+    text = preprocess(text)
+    encoded_input = tokenizer(text, return_tensors='pt')
+    output = model(**encoded_input)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+
+    ranking = np.argsort(scores)
+    ranking = ranking[::-1]
+    result = {}
+    for i in range(scores.shape[0]):
+        l = config.id2label[ranking[i]]
+        s = scores[ranking[i]]
+        result[l] = np.round(float(s), 4)
+
+    return result
