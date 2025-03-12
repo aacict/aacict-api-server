57405cd88f01882068a0cee998d4f8e8e84ef175
Ashish Thapa <aacict@gmail.com>
2025-03-12 13:05:39 -0400

    retrieve key from env

--- newsRetrieve.py
+++ newsRetrieve.py
@@ -1,9 +1,13 @@
 from newsdataapi import NewsDataApiClient
+from dotenv import load_dotenv
+import os
 
-api = NewsDataApiClient(apikey="your_api_key")
+load_dotenv() 
 
-def getNews():
-  response = api.news_api( q= "ronaldo" , country = "us", language="en")
+api = NewsDataApiClient(apikey=os.getenv("NEWDATA_API_KEY"))
+
+def getNews(query='ronaldo', country= 'us', language='en'):
+  response = api.news_api( q = query, country = country, language = language)
   data = []
   for res in response['results']:
     data.append({"title":res['title'], "desc":res['description'] })
