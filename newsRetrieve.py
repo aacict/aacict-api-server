bb175c3f627785bc1f608be43dc3d6cc86b6f33d
Ashish Thapa <aacict@gmail.com>
2025-03-13 14:47:38 -0400

    added docker file and docker compose

--- newsRetrieve.py
+++ newsRetrieve.py
@@ -5,7 +5,7 @@ import os
 load_dotenv() 
 
 
-api = NewsDataApiClient(apikey=os.getenv("NEWDATA_API_KEY"))
+api = NewsDataApiClient(apikey=os.getenv("NEWS_DATA_API_KEY"))
 
 def getNews(query='ronaldo', country= 'us', language='en'):
   response = api.news_api( q = query, country = country, language = language)
