e0bc833f1ef472cfc9dad5dae3ec03e692203945
Ashish Thapa <aacict@gmail.com>
2025-03-18 05:47:19 -0400

    added object detection function and sent new object with bounding box and labels

---
+++ projects/object_detection/objectDetection.py
@@ -0,0 +1,12 @@
+from transformers import DetrImageProcessor, DetrForObjectDetection
+import torch
+processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+
+
+def detect(image):
+        input = processor(images=image, return_tensors="pt")
+        output = model(**input)
+        target_sizes = torch.tensor([image.size[::-1]])
+        result = processor.post_process_object_detection(output, target_sizes=target_sizes, threshold=0.5)[0]
+        return result
--- projects/object_detection/routes.py
+++ projects/object_detection/routes.py
@@ -1,11 +1,40 @@
-from flask import Blueprint, jsonify,request
+from flask import Blueprint, jsonify, request, send_file
+from .objectDetection import detect
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
 
 object_detection_bp = Blueprint('object_detection', __name__)
 
 @object_detection_bp.route('/detect', methods=['POST'])
-def detect():
+def analyze_image():
     try:
-        return jsonify({"status": "success", "data": "news"})
+        if 'image' not in request.files:
+            return jsonify({"error": "No image file provided."}), 400
+        
+        image_file = request.files['image']
+        
+        img = Image.open(BytesIO(image_file.read())).convert("RGB")
+
+        draw = ImageDraw.Draw(img)
+
+        try:
+            font = ImageFont.truetype("arial.ttf", 20) 
+        except IOError:
+            font = ImageFont.load_default()
+
+        result = detect(img)  
+        for score, label, box in zip(result["scores"], result["labels"], result["boxes"]):
+            draw.rectangle(box.tolist(), outline="green", width=2)
+            label_text = f"{model.config.id2label[label.item()]}: {round(score.item(), 2)}"
+            text_position = (box[0], box[1] - 10)
+            draw.text(text_position, label_text, fill="blue", font=font)
+        
+        img_byte_arr = BytesIO()
+        img.save(img_byte_arr, format="JPEG")  # or PNG, if you prefer
+        img_byte_arr.seek(0)
+
+        return send_file(img_byte_arr, mimetype="image/jpeg")
+
     except Exception as e:
         print(e)
         return jsonify({"error": f"An error occurred: {e}"}), 500
